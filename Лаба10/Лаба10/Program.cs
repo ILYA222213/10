d927865a0b8a92d0a932dcf4108bab095fa2ac29
ILYA222213 <117897751+ILYA222213@users.noreply.github.com>
2023-03-24 12:22:53 +0300

    Add files via upload
    Лабораторная 10

---
+++ Лаба10/Лаба10/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            StreamReader fIn = new(@"C:\Users\ilruz\Love.txt");
+
+            string[] elements = fIn.ReadToEnd().Split(',');
+
+            foreach (var i in elements)
+                Console.WriteLine(i);
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (OutOfMemoryException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
